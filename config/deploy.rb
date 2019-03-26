dac6a63cf0383516b6a579e4b741464fda6a7ee5
subaring <subasuba>
2019-03-26 21:13:15 +0900

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "ChatSpace"
+set :application, "ChatSpace2"
 set :repo_url, "git@example.com:me/subasuba/ChatSpace2.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
