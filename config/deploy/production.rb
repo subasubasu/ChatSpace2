2b26a3c2288172cfa7e5b2ec90725c5851523ca0
subaring <subasuba>
2019-03-29 19:56:40 +0900

    Capi

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-# server '52.197.145.92', user: 'ec2-user', roles: %w{app db web}
+server '52.197.145.92', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
